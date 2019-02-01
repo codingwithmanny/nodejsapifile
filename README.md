8026a54aab127c6144e1809608c397799f80878b
m <clkadlaaslk@remotecode.local>
2019-02-01 12:40:21 -0500

    Feat: Initial commit with code and documentation

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# nodejsapifile
-Simple NodeJS API that serves files to download
+# NodeJS API File
+Simple NodeJS API that serves sample files to download and interpret.
+
+## Requirements
+- node 10.15.0
+- yarn 1.13.0 or npm 6.5.0
+
+## Local Environment Instructions
+
+Within directory:
+
+```bash
+yarn start;
+// or npm start;
+```
+
+Port is set to `50011.
+
+## Usage
+
+The rout endpoint expects a query params of `?file=`.
+
+The following are supported formats:
+
+- `?file=csv`
+- `?file=jpg`
+- `?file=png`
+- `?file=pdf`
+- `?file=xlsx`
+
+When one of these requests are made the file will automatically start downloading.
---
+++ index.js
@@ -0,0 +1,46 @@
+// DEPENDENCIES
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = `5001`;
+
+// API ENDPOINTS
+app.get('/', (req, res) => {
+    const fileTypes = [
+        'csv',
+        'jpg',
+        'pdf',
+        'png',
+        'xslx'
+    ];
+
+    // Check if the right request is coming through for the file type
+    return new Promise((resolve, reject) => {
+        if (req.query.file && fileTypes.indexOf(req.query.file.toLowerCase()) > -1) {
+            return resolve(`sample.${fileTypes[fileTypes.indexOf(req.query.file.toLowerCase())]}`);
+        }
+        return reject(`Please profile a file type of ?file=${fileTypes.join('|')}`);
+    })
+    // Validate if the files exists
+    .then((file) => {
+        return new Promise((resolve, reject) => {
+            if(fs.existsSync(`./files/${file}`)) {
+                return resolve(`./files/${file}`)
+            }
+            return reject(`File '${file}' was not found.`);
+        })
+    })
+    // Return the file to download
+    .then((filePath) => {
+        res.download(filePath);
+    })
+    // Catches errors and displays them
+    .catch((e) => {
+        res.status(400).send({
+            message: e,
+        });
+    });
+});
+
+// HTTP SERVER
+app.listen(port, () => console.log(`Listening on port: ${port}`));
